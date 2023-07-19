5cda6c6cda1d5a7d2a9908760a9aff75db09912d
subrahmanyam14 <139866081+subrahmanyam14@users.noreply.github.com>
2023-07-19 16:01:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function bulboff(){
+    document.getElementById("bulb").src="https://images.unsplash.com/photo-1529310399831-ed472b81d589?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGlnaHQlMjBidWxifGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60";
+}
+function bulbon(){
+    document.getElementById("bulb").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhizPRdnTMLYFiA1gYol40-nNChwMUXLJXNQ&usqp=CAU";
+}
